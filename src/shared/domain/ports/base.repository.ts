357bdaaf391716acdffdcf3e74cbc1e363a5b1b8
TakeshiDaveau <takeshi.daveau@prestashop.com>
2024-11-04 16:28:31 +0100

    wip

--- src/shared/domain/ports/base.repository.ts
+++ src/shared/domain/ports/base.repository.ts
@@ -1,18 +1,3 @@
-// export type Database = Record<string, Record<string, any>>;
-// export type TableName<DBSchema extends Database> = keyof DBSchema;
-
-// export type DatabaseSelectable<TDatabase extends Database> = {
-//   [TableName in keyof TDatabase]: TDatabase[TableName]; // Sélection de toutes les colonnes d'une table
-// };
-
-// export type DatabaseInsertable<TDatabase extends Database> = {
-//   [TableName in keyof TDatabase]: Partial<TDatabase[TableName]>;
-// };
-
-// export type DatabaseUpdateable<TDatabase extends Database> = {
-//   [TableName in keyof TDatabase]: Partial<TDatabase[TableName]>;
-// };
-
 export interface BaseRepository<TTableTuple> {
   save(value: TTableTuple): Promise<void>;
   getById(id: string): Promise<TTableTuple | undefined>;
